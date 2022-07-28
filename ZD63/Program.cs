f2933fd382cea118a8edc1d93082ac2ce21edfb9
Starostina <pankovays@mail.ru>
2022-07-28 14:42:45 +0700

    Задача 63 к сминару 9

---
+++ ZD63/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Write("Введите число N: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumbers(int start, int end)
+// 1 - start, end - N
+{
+    // Базовый случай
+    if (start == end) return start.ToString();
+    // Рекурсивный случай
+    return (start + ", " + PrintNumbers(start + 1,end));
+}
+
+System.Console.WriteLine(PrintNumbers(1, N));
