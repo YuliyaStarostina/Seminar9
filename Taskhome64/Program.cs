56e710d7f8164f483dfe0be40732d29aaf9012f7
Starostina <pankovays@mail.ru>
2022-07-28 15:06:15 +0700

    Домашняя работа к семинару 9

---
+++ Taskhome64/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 64: Задайте значения M и N. 
+Напишите программу, которая выведет 
+все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8"" */
+
+Console.WriteLine("Вывести все натуральные числа в промежутке от M до N.");
+Console.WriteLine("Введите значение числа М: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение числа N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(NaturalNumberRecursivno(M,N)); 
+
+string NaturalNumberRecursivno(int numberFirst, int numberSecond)
+{
+    if (numberFirst == numberSecond) return numberSecond.ToString();
+    else return (numberFirst + ", " + NaturalNumberRecursivno(numberFirst+1, numberSecond));
+}
