0963a34e2d13354a30f2820bf3da929005c322cd
Starostina <pankovays@mail.ru>
2022-07-28 14:49:10 +0700

    Задача 67 к семинару 9

---
+++ ZD67/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 67: Напишите программу, 
+// которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int ReturnSum(int n)
+{
+    if (n == 0) return 0;
+    return (n%10) + ReturnSum(n/10);
+}
+Console.Write(ReturnSum(5678943));
+
+///////////////////////
+
+// 67
+
+// int number = Convert.ToInt32(Console.ReadLine());
+// //456
+// // 6, 5, 4 + 0
+// int SumNumbers(int num)
+// {
+//     if (num == 0) return 0;
+//     return (num % 10 + SumNumbers(num / 10));
+// }
+
+// Console.WriteLine(SumNumbers(number));
